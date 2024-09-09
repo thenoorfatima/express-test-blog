9e0d2b2dd872d763aaa39ae3d1cdefd5f72ae0b4
thenoorfatima <noor14fatima@gmail.com>
2024-09-10 01:08:50 +0530

    initial commit

---
+++ data/blogs.js
@@ -0,0 +1,24 @@
+blogs = [
+        {
+            title: "Chocolate Cake",
+            content: "Our special Dutch Chocolate Cake",
+            slug: "chocolate"
+        },
+        {
+            title: "Vanilla Cake",
+            content: "Our special Vanilla Cake",
+            slug: "vanilla"
+        },
+        {
+            title: "Fruit Cake",
+            content: "Our special Fruit Cake",
+            slug: "fruit"
+        },
+        {
+            title: "Strawberry Cake",
+            content: "Our special fresh strawberry Cake",
+            slug: "strawberry"
+        },
+]
+
+module.exports = blogs;
